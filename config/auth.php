f3fb3c55ab3ecfe1b031b0d7e37f1c296dc6c9ac
Ricardo Sauceda <christopherrgs2@hotmail.com>
2025-09-24 13:00:35 -0600

    Feat(auth): agregar soporte para Laravel Passport en el modelo User y configuración de autenticación

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,11 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+
+        'passport' => [
+            'driver' => 'passport',
+            'provider' => 'users',
+        ],
     ],
 
     /*
